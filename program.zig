9a3fcf829b43db20054358d3ff36cf8b8c3de9c4
Jon C <me@jonc.dev>
2023-11-30 17:23:12 +0100

    Make building easier, remove parts that don't work

--- program.zig
+++ program.zig
@@ -1,12 +1,10 @@
 const std = @import("std");
 
-pub fn base58Module(b: *std.build.Builder, comptime base_dir: []const u8) std.build.ModuleDependency {
-    return .{ .name = "base58", .module = b.createModule(.{
+pub fn allSolModules(b: *std.build.Builder, comptime base_dir: []const u8) [6]std.build.ModuleDependency {
+    const base58 = .{ .name = "base58", .module = b.createModule(.{
         .source_file = .{ .path = base_dir ++ "base58/base58.zig" },
     }) };
-}
 
-fn dependentSolModules(b: *std.build.Builder, comptime base_dir: []const u8, base58: std.build.ModuleDependency) [5]std.build.ModuleDependency {
     const bincode = .{ .name = "bincode", .module = b.createModule(.{
         .source_file = .{ .path = base_dir ++ "bincode/bincode.zig" },
     }) };
@@ -40,6 +38,7 @@ fn dependentSolModules(b: *std.build.Builder, comptime base_dir: []const u8, bas
     }) };
 
     return [_]std.build.ModuleDependency{
+        base58,
         bincode,
         borsh,
         sol,
@@ -48,96 +47,81 @@ fn dependentSolModules(b: *std.build.Builder, comptime base_dir: []const u8, bas
     };
 }
 
-pub fn allSolModules(b: *std.build.Builder, comptime base_dir: []const u8) [6]std.build.ModuleDependency {
-    const base58 = base58Module(b, base_dir);
-    const dependent_modules = dependentSolModules(b, base_dir, base58);
+pub fn buildProgram(b: *std.build.Builder, program: *std.build.LibExeObjStep, comptime base_dir: []const u8) !void {
+    const sol_modules = allSolModules(b, base_dir);
 
-    return .{base58} ++ dependent_modules;
-}
-
-fn buildProgramWithTarget(b: *std.build.Builder, target: std.zig.CrossTarget) !void {
-    const base58 = base58Module(b, "");
-    const sol_modules = dependentSolModules(b, "", base58);
-
-    const optimize = .ReleaseSmall;
-    const program = b.addSharedLibrary(.{
-        .name = "main",
-        .root_source_file = .{ .path = "src/main.zig" },
-        .optimize = optimize,
-        .target = target,
-    });
-    program.addModule(base58.name, base58.module);
     inline for (sol_modules) |package| {
         program.addModule(package.name, package.module);
     }
     b.installArtifact(program);
 
     try linkSolanaProgram(b, program);
-    try generateProgramKeypair(b, program, base58);
+    //try generateProgramKeypair(b, program);
 }
 
-pub fn buildProgramV2(b: *std.build.Builder) !void {
-    const sbfv2 = std.Target.sbf.cpu.sbfv2;
-    const target = .{
-        .cpu_arch = .sbf,
-        .cpu_model = .{
-            .explicit = &sbfv2,
-        },
-        .os_tag = .solana,
-        .cpu_features_add = sbfv2.features,
-    };
+pub const sbf_target: std.zig.CrossTarget = .{
+    .cpu_arch = .sbf,
+    .os_tag = .solana,
+    .cpu_features_add = std.Target.sbf.featureSet(&.{.solana}),
+};
 
-    buildProgramWithTarget(b, target);
-}
+pub const sbfv2_target: std.zig.CrossTarget = .{
+    .cpu_arch = .sbf,
+    .cpu_model = .{
+        .explicit = &std.Target.sbf.cpu.sbfv2,
+    },
+    .os_tag = .solana,
+    .cpu_features_add = std.Target.sbf.cpu.sbfv2.features,
+};
 
-pub fn buildProgram(b: *std.build.Builder) !void {
-    const target = .{
-        .cpu_arch = .bpfel,
-        .os_tag = .freestanding,
-        .cpu_features_add = std.Target.bpf.featureSet(&.{.solana}),
-    };
+pub const bpf_target: std.zig.CrossTarget = .{
+    .cpu_arch = .bpfel,
+    .os_tag = .freestanding,
+    .cpu_features_add = std.Target.bpf.featureSet(&.{.solana}),
+};
 
-    buildProgramWithTarget(b, target);
-}
+// TODO how to use base58 within the build without `file exists in multiple modules`?
+// it might be easiest to create a `solana-keygen`-style executable in `base58`
+// and call that from here
+//pub fn generateProgramKeypair(b: *std.build.Builder, lib: *std.build.LibExeObjStep) !void {
+    //const bitcoin = @import("base58/base58.zig").bitcoin;
+    //const path = b.fmt("{s}-keypair.json", .{lib.out_filename[0 .. lib.out_filename.len - std.fs.path.extension(lib.out_filename).len]});
+    //const absolute_path = b.getInstallPath(.lib, path);
 
-pub fn generateProgramKeypair(b: *std.build.Builder, lib: *std.build.LibExeObjStep, base58: std.build.ModuleDependency) !void {
-    const path = b.fmt("{s}-keypair.json", .{lib.out_filename[0 .. lib.out_filename.len - std.fs.path.extension(lib.out_filename).len]});
-    const absolute_path = b.getInstallPath(.lib, path);
+    //if (std.fs.openFileAbsolute(absolute_path, .{})) |keypair_file| {
+        //const keypair_json = try keypair_file.readToEndAlloc(b.allocator, 1 * 1024 * 1024);
+        //const parsed_keypair = try std.json.parseFromSlice([std.crypto.sign.Ed25519.SecretKey.encoded_length]u8, b.allocator, keypair_json, .{});
+        //defer parsed_keypair.deinit();
+        //const keypair_secret = parsed_keypair.value;
 
-    if (std.fs.openFileAbsolute(absolute_path, .{})) |keypair_file| {
-        const keypair_json = try keypair_file.readToEndAlloc(b.allocator, 1 * 1024 * 1024);
-        const parsed_keypair = try std.json.parseFromSlice([std.crypto.sign.Ed25519.SecretKey.encoded_length]u8, b.allocator, keypair_json, .{});
-        defer parsed_keypair.deinit();
-        const keypair_secret = parsed_keypair.value;
+        //const keypair = try std.crypto.sign.Ed25519.KeyPair.fromSecretKey(try std.crypto.sign.Ed25519.SecretKey.fromBytes(keypair_secret));
 
-        const keypair = try std.crypto.sign.Ed25519.KeyPair.fromSecretKey(try std.crypto.sign.Ed25519.SecretKey.fromBytes(keypair_secret));
+        //var program_id_buffer: [bitcoin.getEncodedLengthUpperBound(std.crypto.sign.Ed25519.PublicKey.encoded_length)]u8 = undefined;
+        //const program_id = bitcoin.encode(&program_id_buffer, &keypair.public_key.bytes);
 
-        var program_id_buffer: [base58.module.bitcoin.getEncodedLengthUpperBound(std.crypto.sign.Ed25519.PublicKey.encoded_length)]u8 = undefined;
-        const program_id = base58.module.bitcoin.encode(&program_id_buffer, &keypair.public_key.bytes);
+        //std.debug.print("Program ID: {s}\n", .{program_id});
+    //} else |err| {
+        //if (err != std.fs.File.OpenError.FileNotFound) {
+            //return err;
+        //}
 
-        std.debug.print("Program ID: {s}\n", .{program_id});
-    } else |err| {
-        if (err != std.fs.File.OpenError.FileNotFound) {
-            return err;
-        }
+        //const program_keypair = try std.crypto.sign.Ed25519.KeyPair.create(null);
 
-        const program_keypair = try std.crypto.sign.Ed25519.KeyPair.create(null);
+        //var keypair_json = std.ArrayList(u8).init(b.allocator);
+        //try std.json.stringify(program_keypair.secret_key.bytes, .{}, keypair_json.writer());
 
-        var keypair_json = std.ArrayList(u8).init(b.allocator);
-        try std.json.stringify(program_keypair.secret_key.bytes, .{}, keypair_json.writer());
+        //const keypair = b.addWriteFile(path, keypair_json.items);
+        //b.getInstallStep().dependOn(&keypair.step);
 
-        const keypair = b.addWriteFile(path, keypair_json.items);
-        b.getInstallStep().dependOn(&keypair.step);
+        //const install_keypair = b.addInstallLibFile(keypair.files.items[0].getPath(), path);
+        //b.getInstallStep().dependOn(&install_keypair.step);
 
-        const install_keypair = b.addInstallLibFile(keypair.files.items[0].getPath(), path);
-        b.getInstallStep().dependOn(&install_keypair.step);
+        //var program_id_buffer: [bitcoin.getEncodedLengthUpperBound(std.crypto.sign.Ed25519.PublicKey.encoded_length)]u8 = undefined;
+        //const program_id = bitcoin.encode(&program_id_buffer, &program_keypair.public_key.bytes);
 
-        var program_id_buffer: [base58.module.bitcoin.getEncodedLengthUpperBound(std.crypto.sign.Ed25519.PublicKey.encoded_length)]u8 = undefined;
-        const program_id = base58.module.bitcoin.encode(&program_id_buffer, &program_keypair.public_key.bytes);
-
-        std.debug.print("Program ID: {s}\n", .{program_id});
-    }
-}
+        //std.debug.print("Program ID: {s}\n", .{program_id});
+    //}
+//}
 
 pub fn linkSolanaProgram(b: *std.build.Builder, lib: *std.build.LibExeObjStep) !void {
     const linker_script = b.addWriteFile("bpf.ld",
