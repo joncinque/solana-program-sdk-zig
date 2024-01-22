4818dbbee2736f9e1f571992ab17447c4b652e44
Jon C <me@jonc.dev>
2024-01-22 14:44:48 +0100

    Update slot_hashes to avoid `@This`

--- slot_hashes.zig
+++ slot_hashes.zig
@@ -1,22 +1,22 @@
 const std = @import("std");
 const PublicKey = @import("public_key.zig").PublicKey;
 
-const SlotHashes = @This();
+pub const SlotHashes = struct {
+    ptr: [*]SlotHash,
+    len: u64,
 
-pub const id = PublicKey.comptimeFromBase58("SysvarS1otHashes111111111111111111111111111");
+    pub const id = PublicKey.comptimeFromBase58("SysvarS1otHashes111111111111111111111111111");
 
-/// About 2.5 minutes to get your vote in.
-pub const max_entries = 512;
+    /// About 2.5 minutes to get your vote in.
+    pub const max_entries = 512;
+};
 
 pub const SlotHash = struct {
     slot: u64,
     hash: [32]u8,
+
+    pub fn from(data: []const u8) []const SlotHash {
+        const len = std.mem.readIntSliceLittle(u64, data[0..@sizeOf(u64)]);
+        return @as([*]const SlotHash, @ptrCast(@alignCast(data.ptr + @sizeOf(u64))))[0..len];
+    }
 };
-
-ptr: [*]SlotHash,
-len: u64,
-
-pub fn from(data: []const u8) []const SlotHash {
-    const len = std.mem.readIntSliceLittle(u64, data[0..@sizeOf(u64)]);
-    return @as([*]const SlotHash, @ptrCast(@alignCast(data.ptr + @sizeOf(u64))))[0..len];
-}
