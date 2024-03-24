a9c9216dfbfe90cdbc337dd2185902716d560e39
Jon C <me@jonc.dev>
2024-03-24 19:55:45 +0100

    program-test: Update test.sh for CI

--- program-test/test.sh
+++ program-test/test.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-set -e
-../../zig-native-linux-gnu-native/zig build --summary all
-SBF_OUT_DIR=./zig-out/lib cargo test
+ZIG="$1"
+ROOT_DIR="$(cd "$(dirname "$0")"/..; pwd)"
+if [[ -z "$ZIG" ]]; then
+  ZIG="$ROOT_DIR/../zig-x86_64-linux-gnu-baseline/zig"
+fi
 
+set -e
+cd $ROOT_DIR/program-test
+$ZIG build --summary all --verbose --global-cache-dir zig-global-cache
+SBF_OUT_DIR="$ROOT_DIR/program-test/zig-out/lib" cargo test --manifest-path "$ROOT_DIR/program-test/Cargo.toml"
