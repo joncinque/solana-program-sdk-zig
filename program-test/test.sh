e4b245891b7d82e367cd04245afeb26d9295bde0
Jon C <me@jonc.dev>
2024-03-24 20:06:58 +0100

    CI: Limit build and test jobs

--- program-test/test.sh
+++ program-test/test.sh
@@ -8,5 +8,5 @@ fi
 
 set -e
 cd $ROOT_DIR/program-test
-$ZIG build --summary all --verbose --global-cache-dir zig-global-cache
+$ZIG build --summary all --verbose --global-cache-dir zig-global-cache -j1
 SBF_OUT_DIR="$ROOT_DIR/program-test/zig-out/lib" cargo test --manifest-path "$ROOT_DIR/program-test/Cargo.toml"
