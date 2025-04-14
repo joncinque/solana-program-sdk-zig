26ec1ea9c2aca1e1332f813c7005e4247de36532
Jon C <me@jonc.dev>
2025-04-14 13:44:41 +0200

    Move the installation into GH actions

--- program-test/install-build-deps.sh
+++ program-test/install-build-deps.sh
@@ -2,11 +2,6 @@
 set -e
 case $(uname -s | cut -c1-7) in
 "Windows" | "MINGW64")
-  export PERL="$(which perl)"
-  export OPENSSL_SRC_PERL="$(which perl)"
-  choco install openssl --version 3.4.1 --install-arguments="'/DIR=C:\OpenSSL'" -y
-  export OPENSSL_LIB_DIR='C:\OpenSSL\lib\VC\x64\MT'
-  export OPENSSL_INCLUDE_DIR='C:\OpenSSL\include'
   choco install protoc
   export PROTOC='C:\ProgramData\chocolatey\lib\protoc\tools\bin\protoc.exe'
   ;;
