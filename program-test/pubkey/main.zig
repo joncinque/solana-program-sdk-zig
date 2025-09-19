99c8fce2b4a1de29a0b21fd8111e5e09eba5b8e0
Davirain <davirain.yin@gmail.com>
2025-09-19 23:39:55 +0800

    Use export entrypoint function

--- program-test/pubkey/main.zig
+++ program-test/pubkey/main.zig
@@ -3,11 +3,11 @@ const sol = @import("solana_program_sdk");
 fn processInstruction(program_id: *sol.PublicKey, accounts: []sol.Account, data: []const u8) sol.ProgramError!void {
     _ = accounts;
     _ = data;
-    sol.print("Hello New processInstruction program {s}", .{program_id});
+    sol.print("Hello zig program {s}", .{program_id});
     return;
 }
 
 // Declare the program entrypoint
 comptime {
-    sol.declareEntrypoint(processInstruction);
+    sol.entrypoint(processInstruction);
 }
