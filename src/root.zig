83c66213e948d06af87e6259c5073fa4de0a52e9
Jon C <me@jonc.dev>
2025-10-27 21:12:48 +0100

    Update to Zig 0.15 (#17)
    #### Problem
    
    Zig 0.15 is out, but the package is still on 0.14.
    
    #### Summary of changes
    
    Now that the solana-zig compiler supports 0.15, update everything!
    
    BREAKING CHANGES:
    * since `using namespace` was removed, do the right thing and import
      into `root.zig`, so imports will need a full path, so `sol.PublicKey`
      should become `sol.public_key.PublicKey`
    * update `callconv(.C)` -> `callconv(.c)`
    * change `format` definition to match new writer API
    * get format strings right: `"{}"` -> `"{f}"`
    * update program-test to mollusk, update other deps

--- src/root.zig
+++ src/root.zig
@@ -1,31 +1,28 @@
 const std = @import("std");
 
-const sol = @This();
-
-pub usingnamespace @import("public_key.zig");
-pub usingnamespace @import("account.zig");
-pub usingnamespace @import("instruction.zig");
-pub usingnamespace @import("allocator.zig");
-pub usingnamespace @import("context.zig");
-pub usingnamespace @import("clock.zig");
-pub usingnamespace @import("rent.zig");
-pub usingnamespace @import("log.zig");
-pub usingnamespace @import("hash.zig");
+pub const public_key = @import("public_key.zig");
+pub const account = @import("account.zig");
+pub const instruction = @import("instruction.zig");
+pub const allocator = @import("allocator.zig");
+pub const context = @import("context.zig");
+pub const clock = @import("clock.zig");
+pub const rent = @import("rent.zig");
+pub const log = @import("log.zig");
+pub const hash = @import("hash.zig");
 
 pub const blake3 = @import("blake3.zig");
-//pub const system_program = @import("system_program.zig");
 pub const slot_hashes = @import("slot_hashes.zig");
 
 pub const bpf = @import("bpf.zig");
 
-pub const native_loader_id = sol.PublicKey.comptimeFromBase58("NativeLoader1111111111111111111111111111111");
-pub const incinerator_id = sol.PublicKey.comptimeFromBase58("1nc1nerator11111111111111111111111111111111");
+pub const native_loader_id = public_key.PublicKey.comptimeFromBase58("NativeLoader1111111111111111111111111111111");
+pub const incinerator_id = public_key.PublicKey.comptimeFromBase58("1nc1nerator11111111111111111111111111111111");
 
-pub const sysvar_id = sol.PublicKey.comptimeFromBase58("Sysvar1111111111111111111111111111111111111");
-pub const instructions_id = sol.PublicKey.comptimeFromBase58("Sysvar1nstructions1111111111111111111111111");
+pub const sysvar_id = public_key.PublicKey.comptimeFromBase58("Sysvar1111111111111111111111111111111111111");
+pub const instructions_id = public_key.PublicKey.comptimeFromBase58("Sysvar1nstructions1111111111111111111111111");
 
-pub const ed25519_program_id = sol.PublicKey.comptimeFromBase58("Ed25519SigVerify111111111111111111111111111");
-pub const secp256k1_program_id = sol.PublicKey.comptimeFromBase58("KeccakSecp256k11111111111111111111111111111");
+pub const ed25519_program_id = public_key.PublicKey.comptimeFromBase58("Ed25519SigVerify111111111111111111111111111");
+pub const secp256k1_program_id = public_key.PublicKey.comptimeFromBase58("KeccakSecp256k11111111111111111111111111111");
 
 pub const lamports_per_sol = 1_000_000_000;
 
