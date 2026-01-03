f17f9e81e028ca1e3aaf32c4b46ab0a09596c673
DaviRain-Su <davirain.yin@gmail.com>
2026-01-03 14:37:12 +0800

    fix program_id print on program-test pubkey

--- program-test/pubkey/main.zig
+++ program-test/pubkey/main.zig
@@ -3,8 +3,7 @@ const sol = @import("solana_program_sdk");
 fn processInstruction(program_id: *sol.PublicKey, accounts: []sol.Account, data: []const u8) sol.ProgramError!void {
     _ = accounts;
     _ = data;
-    _ = program_id;
-    sol.print("Hello zig program", .{});
+    sol.print("Hello zig program: {f}", .{program_id});
     return;
 }
 
